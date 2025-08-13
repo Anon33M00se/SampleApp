9a7f6fb17c60d73992440c8db14acb646549805e
Modiot Labs <xcode@modiot.com>
2025-08-13 19:22:17 -0400

    Changes to support the turbo module shared stuff

---
+++ ios/NativeSampleModuleProvider.h
@@ -0,0 +1,18 @@
+//
+//  NativeSampleModuleProvider.h
+//  SampleApp
+//
+//  Created by Anon Emoose on 8/13/25.
+//
+
+
+#import <Foundation/Foundation.h>
+#import <ReactCommon/RCTTurboModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NativeSampleModuleProvider : NSObject <RCTModuleProvider>
+
+@end
+
+NS_ASSUME_NONNULL_END
